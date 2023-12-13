4fd4421e15e1b59a8cb28da5769ef7c56cbf6f46
SaiTeja-002 <teja00219@gmail.com>
2023-12-13 20:15:35 +0530

    Spacing Bug Fixed

--- frontend/src/pages/VisitProfile.jsx
+++ frontend/src/pages/VisitProfile.jsx
@@ -30,68 +30,46 @@ const VisitProfile = ({ name }) => {
     }
 
     return (
-        <div>
-            <div style={{ height: "100vh", padding: "15px" }}>
-                <div className="profile-container">
-                    <div className="left-column">
-                        <div className="profile-info">
-                            <img src={profile.photo} alt="Profile Picture" className="profile-image" />
-                            <h2>{profile.name}</h2>
-                            <h3>{profile.bio}</h3>
-                        </div>
-                    </div>
-                    <div className="right-column">
-                        <div className="counts-section">
-                            <div className="count-group">
-                                <div className="count-item">
-                                    <p>{posts.length}</p>
-                                    <h3>Posts</h3>
-                                </div>
-                                <div className="count-item">
-                                    <p>{profile.followerCount}</p>
-                                    <h3>Followers</h3>
-                                </div>
-                                <div className="count-item">
-                                    <p>{profile.followingCount}</p>
-                                    <h3>Following</h3>
-                                </div>
-                            </div>
-                            {profile.isFollowing ? (
-                                <button className="unfollow-button">Following</button>
-                            ) : (
-                                <button className="button" onClick={followUser}>Follow</button>
-                            )}
-                        </div>
+        <div style={{ height: "100vh", padding: "15px" }}>
+            <div className="profile-container">
+                {/* <div>hello</div> */}
+                <div className="left-column">
+                    {/* <div>hello left-column</div> */}
+                    <div className="profile-info">
+                        <img src={profile.photo} alt="Profile Picture" className="profile-image" />
+                        <h2>{profile.name}</h2>
+                        <h3>{profile.bio}</h3>
                     </div>
                 </div>
-
-                <PostLayout feed={posts} />
-
+                <div className="right-column">
+                    <div className="counts-section">
+                        <div className="count-group">
+                            <div className="count-item">
+                                <p>{posts.length}</p>
+                                <h3>Posts</h3>
+                            </div>
+                            <div className="count-item">
+                                <p>{profile.followerCount}</p>
+                                <h3>Followers</h3>
+                            </div>
+                            <div className="count-item">
+                                <p>{profile.followingCount}</p>
+                                <h3>Following</h3>
+                            </div>
+                        </div>
+                        {profile.isFollowing ? (
+                            <button className="unfollow-button">Following</button>
+                        ) : (
+                            <button className="button" onClick={followUser}>Follow</button>
+                        )}
+                    </div>
+                </div>
             </div>
+
+            <PostLayout data-testid="post-layout" feed={posts}/>
+
         </div>
     );
 };
 
 export default VisitProfile;
-
-
-{/* <div className="image-gallery">
-<ResponsiveMasonry
-    columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
->
-    <Masonry>
-        {profile.posts.map((image, i) => (
-            <div key={i} style={{ margin: "10px 10px" }}>
-                <img
-                    src={image}
-                    style={{
-                        width: "100%",
-                        display: "block",
-                    }}
-                    alt="img"
-                />
-            </div>
-        ))}
-    </Masonry>
-</ResponsiveMasonry>
-</div> */}
