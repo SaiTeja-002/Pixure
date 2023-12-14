138a0a930a3f517518398d53aa318433376ab01e
SaiTeja-002 <teja00219@gmail.com>
2023-12-15 00:54:27 +0530

    Refactored LoginPage

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React, {  useState } from 'react';
+import React, { useState } from 'react';
 import { TextField } from '@mui/material';
 import '../styles/Login.css'
 import * as authActions from '../actions/authAction.js';
